2a439fb8594d4d3666f97bed2738219e697c05b5
yezzi320 <yezzi320@yonsei.ac.kr>
2025-07-17 19:42:28 +0900

    Update user_router.py

--- app/user/user_router.py
+++ app/user/user_router.py
@@ -36,5 +36,8 @@ def delete_user(user_delete_request: UserDeleteRequest, service: UserService = D
 
 @user.put("/update-password", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def update_user_password(user_update: UserUpdate, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
-    ## TODO
-    return None
+    try:
+        updated_user = service.update_user_pwd(user_update)
+        return BaseResponse(status="success", data=updated_user, message="User password update success.")
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
