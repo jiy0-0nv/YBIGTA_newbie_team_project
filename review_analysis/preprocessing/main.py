667f8f29cc297ea57d59f5971dd9662462297889
jinwook <mech1011@naver.com>
2025-07-24 21:22:57 +0900

    Feat: metacritic 전처리 구현

--- review_analysis/preprocessing/main.py
+++ review_analysis/preprocessing/main.py
@@ -13,13 +13,15 @@ from review_analysis.preprocessing.imdb_processor import IMDBProcessor
 # key는 "reviews_사이트이름"으로, value는 해당 처리를 위한 클래스
 PREPROCESS_CLASSES: Dict[str, Type[BaseDataProcessor]] = {
     "reviews_example": ExampleProcessor,
-    "metacritic_reviews" : MetacriticProcessor,
+    "reviews_metacritic" : MetacriticProcessor,
     "reviews_rottentomatoes": RottentomatoesProcessor,
     "reviews_imdb": IMDBProcessor
     # key는 크롤링한 csv파일 이름으로 적어주세요! ex. reviews_naver.csv -> reviews_naver
 }
 
-REVIEW_COLLECTIONS = glob.glob(os.path.join("..","..","database", "reviews_*.csv"))
+# 파일위치 기준 탐색
+script_dir = os.path.dirname(os.path.abspath(__file__))
+REVIEW_COLLECTIONS = glob.glob(os.path.join(script_dir,"..","..","database", "reviews_*.csv"))
 
 def create_parser() -> ArgumentParser:
     parser = ArgumentParser()
@@ -34,15 +36,14 @@ if __name__ == "__main__":
 
     parser = create_parser()
     args = parser.parse_args()
+    # 파일위치기준 경로설정
+    args.output_dir = os.path.join(script_dir,args.output_dir)
 
     os.makedirs(args.output_dir, exist_ok=True)
-
     if args.all: 
         for csv_file in REVIEW_COLLECTIONS:
-            print("sdd1")
             base_name = os.path.splitext(os.path.basename(csv_file))[0]
             if base_name in PREPROCESS_CLASSES:
-                print("sdd2")
                 preprocessor_class = PREPROCESS_CLASSES[base_name]
                 preprocessor = preprocessor_class(csv_file, args.output_dir)
                 preprocessor.preprocess()
--- review_analysis/preprocessing/metacritic_processor.py
+++ review_analysis/preprocessing/metacritic_processor.py
@@ -2,6 +2,7 @@ from review_analysis.preprocessing.base_processor import BaseDataProcessor
 import pandas as pd
 import re
 import emoji
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 import os
 
@@ -33,7 +34,7 @@ class MetacriticProcessor(BaseDataProcessor):
         if pd.isna(text):
             return text
         # emoji 라이브러리 사용
-        return emoji.replace_emojis(text, replace='')
+        return emoji.replace_emoji(text, replace='')
 
     def feature_engineering(self):
         '''
@@ -67,12 +68,12 @@ class MetacriticProcessor(BaseDataProcessor):
         # 원본 데이터프레임에 벡터 컬럼들 추가
         self.df = pd.concat([self.df, vector_df], axis=1)
         
-        print("피처 엔지니어링 완료!")
-        print(f"새로 생성된 컬럼 수: {len(vector_df.columns) + 1}")  # hatescore + 벡터 컬럼들        
+        # print("피처 엔지니어링 완료!")
+        # print(f"새로 생성된 컬럼 수: {len(vector_df.columns) + 1}")  # hatescore + 벡터 컬럼들        
 
     def save_to_database(self):
         output_filename = "preprocessed_reviews_metacritic.csv"
         output_path = os.path.join(self.output_dir, output_filename)
         self.df.to_csv(output_path, index=False, encoding='utf-8-sig')
-        print("저장 완료!")
+        # print("저장 완료!")
 
